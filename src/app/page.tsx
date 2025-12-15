38db02cfd5e4427e0c3492c624dfdd63d5d9396f
Ram Dattu <ramdattu54@gmail.com>
2025-12-15 16:39:23 +0000

    loop the song

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <div className="w-full h-full relative">
         <InfiniteMenu items={items}/>
       </div>
-      <audio autoPlay>
+      <audio autoPlay loop>
         <source src="https://ik.imagekit.io/bhanuteja110/New%20folder/Oggy%20and%20the%20Cockroaches%20-%20ALL%20OPENINGS%20_%201998%20-%202018%20low.mp3" type="audio/mpeg" />
       </audio>
     </main>
